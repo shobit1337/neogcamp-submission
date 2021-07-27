234a3ce07ee8a4f16f479ee0ca1906d0040e52f8
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-07-27 23:37:01 +0530

    refactor: markTen

--- markTen-cash-register/src/App.jsx
+++ markTen-cash-register/src/App.jsx
@@ -2,43 +2,43 @@ import React, { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const [billAmount, setbillAmount] = useState(0);
-  const billValue = useRef(0);
-  const handleBill = (e) => {
-    e.preventDefault();
-    console.log(billValue.current.innerHtml);
-    if (billValue > 0) {
-      setbillAmount(billValue);
-    }
-    const handleCash = () => {};
-  };
-  return (
-    <div className="App">
-      <header className="register-container">
-        <h2>Cash Resgister Manager</h2>
-        <p>
-          Enter the bill ammount and cash given by the customer and know minimum
-          number of notes to return.
-        </p>
-        <label htmlFor="bill-amount">Bill Amount:</label>
-        <input
-          ref={billValue}
-          type="number"
-          name="bill-amount"
-          id="bill-input"
-          required
-        />
-        {!billAmount && <button onClick={handleBill}>Next</button>}
-        {billAmount && (
-          <div>
-            <label htmlFor="Cash Given:"></label>
-            <input type="number" name="cash-given" id="cash-input" required />
-            <button onClick={handleCash}>Check</button>
-          </div>
-        )}
-      </header>
-    </div>
-  );
+  // const [billAmount, setbillAmount] = useState(0);
+  // const billValue = useRef(0);
+  // const handleBill = (e) => {
+  //   e.preventDefault();
+  //   console.log(billValue.current.innerHtml);
+  //   if (billValue > 0) {
+  //     setbillAmount(billValue);
+  //   }
+  //   const handleCash = () => {};
+  // };
+  // return (
+  //   <div className="App">
+  //     <header className="register-container">
+  //       <h2>Cash Resgister Manager</h2>
+  //       <p>
+  //         Enter the bill ammount and cash given by the customer and know minimum
+  //         number of notes to return.
+  //       </p>
+  //       <label htmlFor="bill-amount">Bill Amount:</label>
+  //       <input
+  //         ref={billValue}
+  //         type="number"
+  //         name="bill-amount"
+  //         id="bill-input"
+  //         required
+  //       />
+  //       {!billAmount && <button onClick={handleBill}>Next</button>}
+  //       {billAmount && (
+  //         <div>
+  //           <label htmlFor="Cash Given:"></label>
+  //           <input type="number" name="cash-given" id="cash-input" required />
+  //           <button onClick={handleCash}>Check</button>
+  //         </div>
+  //       )}
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
