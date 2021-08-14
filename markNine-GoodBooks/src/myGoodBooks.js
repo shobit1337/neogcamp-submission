a16940bb92797c9ec3595bcf828c65e995ed28c5
Shobit Deshwal <shobitdeshwal1337@gmail.com>
2021-08-15 01:30:25 +0530

    feat: added more books

--- markNine-GoodBooks/src/myGoodBooks.js
+++ markNine-GoodBooks/src/myGoodBooks.js
@@ -2,23 +2,79 @@ export default [
   {
     genre: "JavaScript",
     books: [
-      { name: "Eloquent JavaScript", stars: 5, link: "" },
-      { name: "You-Dont-Know-JS", stars: 4, link: "" },
+      {
+        name: "Eloquent JavaScript",
+        stars: 5,
+        link: "https://www.amazon.in/Eloquent-JavaScript-3rd-Introduction-Programming/dp/1593279507/",
+        discription:
+          "JavaScript lies at the heart of almost every modern web application, from social apps like Twitter to browser-based game frameworks like Phaser and Babylon. Though simple for beginners to pick up and play with, JavaScript is a flexible, complex language that you can use to build full-scale applications.",
+      },
+      {
+        name: "You-Dont-Know-JS",
+        stars: 4,
+        link: "https://www.amazon.in/You-Dont-Know-Set-Volumes/dp/9352136268/",
+        discription:
+          'No matter how much experience you have with JavaScript, odds are you don’t fully understand the language. The "You Don’t Know JS" is a series of books which explores JavaScript types in greater depth',
+      },
+      {
+        name: "JavaScript: The Definitive Guide",
+        stars: 4,
+        link: "https://www.amazon.in/dp/0596805527",
+        discription:
+          "Since 1996, JavaScript: The Definitive Guide has been the bible for JavaScript programmers—a programmer's guide and comprehensive reference to the core language and to the client-side JavaScript APIs defined by web browsers.",
+      },
     ],
   },
   {
     genre: "Backend",
     books: [
-      { name: "Designing Data-Intensive application", stars: 5, link: "" },
-      { name: "Designing Distributed systems", stars: 4, link: "" },
-      { name: "Data communications and Networking", stars: 4, link: "" },
+      {
+        name: "Designing Data-Intensive application",
+        stars: 5,
+        link: "https://www.amazon.in/Designing-Data-Intensive-Applications-Reliable-Maintainable/dp/9352135245/",
+        discription:
+          "All Indian Reprints of O'Reilly are printed in Grayscale. Data is at the center of many challenges in system design today. Difficult issues need to be figured out, such as scalability, consistency, reliability, efficiency, and maintainability.",
+      },
+      {
+        name: "Designing Distributed systems",
+        stars: 4,
+        link: "https://www.amazon.in/Designing-Distributed-Systems-Brendan-Burns/dp/1491983647/",
+        discription:
+          "In the race to compete in today’s fast-moving markets, large enterprises are busy adopting new technologies for creating new products, processes, and business models. But one obstacle on the road to digital transformation is placing too much emphasis on technology, and not enough on the types of processes technology enables.",
+      },
+      {
+        name: "Data communications and Networking",
+        stars: 4,
+        link: "https://www.amazon.in/Data-Communications-Networking-Forouzan/dp/1259064751/",
+        discription:
+          " It has been developed specifically to meet the need of students of computer networks. In addition to a chapter on the peer-to-peer paradigm, a full chapter on quality of service (QoS), generous coverage of forward error correction, coverage of WiMAX and material on socket-interface programming in Java, we have added new end-of-chapter questions and problems to make the content more relevant and improve learning outcomes for the student.",
+      },
     ],
   },
   {
     genre: "DSA",
     books: [
-      { name: "Introduction To Algorithms", stars: 5, link: "" },
-      { name: "Cracking The Coding Interview", stars: 4, link: "" },
+      {
+        name: "Introduction To Algorithms",
+        stars: 5,
+        link: "https://www.amazon.in/Introduction-Algorithms-Eastern-Economy-Thomas/dp/8120340078/",
+        discription:
+          "This internationally acclaimed textbook provides a comprehensive introduction to the modern study of computer algorithms. It covers a broad range of algorithms in depth, yet makes their design and analysis accessible to all levels of readers. Each chapter is relatively selfcontained and presents an algorithm, a design technique, an application area, or a related topic.",
+      },
+      {
+        name: "Cracking The Coding Interview",
+        stars: 4,
+        link: "https://www.amazon.in/Cracking-the-Coding-Interview/dp/0984782869/",
+        discription:
+          "Gayle laakmann McDowell is the founder and CEO of careercup and the author of cracking the PM interview and cracking the tech career. Her background is in software development. She has worked as a software engineer at Google, Microsoft, and Apple. ",
+      },
+      {
+        name: "Data Structures And Algorithms Made Easy",
+        stars: 4,
+        link: "https://www.amazon.in/Data-Structures-Algorithms-Made-Easy/dp/819324527X/",
+        discription:
+          "Reading books is a kind of enjoyment. Reading books is a good habit. We bring you a different kinds of books. You can carry this book where ever you want. It is easy to carry. It can be an ideal gift to yourself and to your loved ones. Care instruction keep away from fire.",
+      },
     ],
   },
   {
@@ -27,20 +83,50 @@ export default [
       {
         name: "Operating System Concepts by Silberschatz",
         stars: 5,
-        link: "",
+        link: "https://www.amazon.in/Operating-System-Concepts-Abraham-Silberschatz/dp/1118129385/",
+        discription:
+          "Operating System Concepts, now in its ninth edition, continues to provide a solid theoretical foundation for understanding operating systems. The ninth edition has been thoroughly updated to include contemporary examples of how operating systems function.The text includes content to bridge the gap between concepts andactual implementations",
       },
       {
         name: "Cryptography and Network Security By Pearson",
         stars: 4,
-        link: "",
+        link: "https://www.amazon.in/Cryptography-Network-Security-Principles-Practice/dp/9332585229/",
+        discription:
+          "Pearson brings to you the revised edition of Cryptography and Network Security by Stallings. In an age of viruses and hackers, electronic eavesdropping and electronic fraud on a global scale, security is paramount. The purpose of this book is to provide a practical survey of both the principles and practice of cryptography and network security.",
+      },
+      {
+        name: "Clean Code",
+        stars: 4,
+        link: "https://www.amazon.in/Clean-Code-Robert-C-Martin/dp/8131773388/",
+        discription:
+          "Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way.",
       },
     ],
   },
   {
     genre: "Motivational",
     books: [
-      { name: "Atomic Habits", stars: 5, link: "" },
-      { name: "The Subtle Art of Not Giving A F*ck", stars: 4, link: "" },
+      {
+        name: "Atomic Habits",
+        stars: 5,
+        link: "https://www.amazon.in/Atomic-Habits-James-Clear/dp/1847941834/",
+        discription:
+          "World-renowned habits expert James Clear has discovered a simpler system for transforming your life. He knows that lasting change comes from the compound effect of hundreds of small decisions – doing two push-ups a day, waking up five minutes early, or holding a single short phone call. He calls them atomic habits.",
+      },
+      {
+        name: "The Subtle Art of Not Giving A F*ck",
+        stars: 4,
+        link: "https://www.amazon.in/Subtle-Art-Not-Giving/dp/0062641549",
+        discription:
+          "In this generation-defining self-help guide, a superstar blogger cuts through the crap to show us how to stop trying to be “positive” all the time so that we can truly become better, happier people",
+      },
+      {
+        name: "Ikigai",
+        stars: 4,
+        link: "https://www.amazon.in/Ikigai-H%C3%A9ctor-Garc%C3%ADa/dp/178633089X/",
+        discription:
+          "Finding your ikigai is easier than you might think. This book will help you work out what your own ikigai really is, and equip you to change your life. You have a purpose in this world: your skills, your interests, your desires and your history have made you the perfect candidate for something. All you have to do is find it.",
+      },
     ],
   },
 ];
